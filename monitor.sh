9a8d8686aa98d991d2b9f3ad1e708f9199f049d4
endto <425967995@qq.com>
2018-05-02 19:42:48 +0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-printf "Date\t\t\tMemory\t\tDisk\t\tCPU\t\tGPU\t\tGPU Memory\n"
+printf "Memory\t\tDisk\t\tCPU\t\tGPU\t\tGPU Memory\t\tDate\n"
 end=$((SECONDS+3600))
 while [ $SECONDS -lt $end ]; do
 Date=$(date -d today +"%Y-%m-%d %T" | awk '{printf "%s %s\t\t\t", $1, $2}')
@@ -8,6 +8,6 @@ DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
 CPU=$(top -bn1 | grep load | awk '{printf "%.2f%%\t\t", $(NF-2)}')
 GPU=$(nvidia-smi --query-gpu=utilization.gpu --format=csv | awk 'NR==2{printf "%s%s\t\t", $1, $2}')
 GPU_MEM=$(nvidia-smi --query-gpu=memory.used --format=csv | awk 'NR==2{printf "%.2f\t\t", $1/1024}')
-echo "$Date$MEMORY$DISK$CPU$GPU$GPU_MEM"
+echo "$MEMORY$DISK$CPU$GPU$GPU_MEM$Date"
 sleep 1
 done
